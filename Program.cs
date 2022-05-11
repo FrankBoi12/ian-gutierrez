396c788593eaf1b7c224b0a2677f60be95dc6ec3
FrankBoi12 <105332560+FrankBoi12@users.noreply.github.com>
2022-05-10 23:41:08 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Dou");
+
+var Condicion = "S";
+
+while (Condicion == "S" )
+{
+    Console.Clear();
+
+    Console.WriteLine("Introduze tu nombre:");
+   var nombre = Console.ReadLine();
+    Console.WriteLine("Hola " + nombre);
+
+    Console.WriteLine("Quieres continuar? S/N");
+    Condicion = Console.ReadLine().ToUpper();
+
+    if (Condicion == "N")
+    {
+        Console.WriteLine("Programa finalizado correctamente");
+      
+    }
+   
+    else 
+    {
+        Console.WriteLine("Opcion no valida");
+        
+    }
+}
+
